0207df1b10c3978c8b0fc766dc129406c82f8247
sanvi <agarwass@rose-hulman.edu>
2025-09-25 12:04:28 -0400

    fix

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ function dropHandler(ev){
 let description_two = document.getElementById("description_two");
 let two = document.getElementById("two");
 two.addEventListener("click", function() {
-    description_two.innerText="We started by making a box and pointer diagram";
+    description_two.textContent="We started by making a box and pointer diagram";
 });
 let description_three = document.getElementById("description_three");
 let three = document.getElementById("three");
 three.addEventListener("click", function() {
-    description_three.innerText="We started by choosing a data set";
+    description_three.textContent="We started by choosing a data set";
 });
 let description_one = document.getElementById("description_one");
 let one = document.getElementById("one");
 one.addEventListener("click", function() {
-    description_one.innerText="We started by making a box and pointer diagram";
+    description_one.textContent="We started by making a box and pointer diagram";
 });
 
