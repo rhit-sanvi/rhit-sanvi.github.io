3143ea4a1f731064127fc42fb8f5e269f26712c0
sanvi <agarwass@rose-hulman.edu>
2025-09-25 09:35:31 -0400

    added all button func

--- script.js
+++ script.js
@@ -14,4 +14,15 @@ let description_two = document.getElementById("description_two");
 let two = document.getElementById("two");
 two.addEventListener("click", function() {
     description_two.textContent="We started by making a box and pointer diagram";
-})
+});
+let description_three = document.getElementById("description_three");
+let three = document.getElementById("three");
+three.addEventListener("click", function() {
+    description_three.textContent="We started by choosing a data set";
+});
+let description_one = document.getElementById("description_one");
+let one = document.getElementById("one");
+one.addEventListener("click", function() {
+    description_one.textContent="We started by making a box and pointer diagram";
+});
+
